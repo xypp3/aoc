2e1c4e403c617a9331fd65f8854344bbd2a006bf
xypp3 <petrkmilev@gmail.com>
2024-12-06 12:26:14 +0000

    day2 done

--- 2024/day_02/bin/main.ml
+++ 2024/day_02/bin/main.ml
@@ -1,4 +1,5 @@
 open Stdlib
+open Printf
 
 let () = print_endline "Hello, World!";;
 
@@ -13,9 +14,49 @@ let testStr =
 let make_reports str : int list list=
         let lines = String.split_on_char '\n' str in
         let nums_f l = List.map (fun c -> int_of_string c) l in
-        List.map (fun l -> nums_f (String.split_on_char ' ' l) ) lines;;
+        List.map (fun l -> nums_f (String.split_on_char ' ' l)) lines;;
+
+
+let report_is_safe report : bool =
+        let f a b = if (List.hd report) < (List.hd (List.tl report)) then (a < b && a+3 >= b) else (a > b && a-3 <= b) in
+        let rec pairs r = 
+                match r with
+                | x :: y :: _ -> if f x y then pairs (List.tl r) else false
+                | _ -> true
+        in
+        pairs report;;
+
+let count_safe reports = 
+        List.length (List.filter (fun r -> report_is_safe r) reports);;
 
 
 (* print test str *)
-let () = List.iter (fun x ->  List.iter (fun y -> Printf.printf "%d;" y) x; Printf.printf "\n") (make_reports testStr);;
+let () = List.iter (fun x ->  List.iter (fun y -> printf "%d;" y) x; printf "\n") (make_reports testStr);;
+let () = printf "%B\n" (report_is_safe (List.hd (List.tl (make_reports testStr))));;
+let () = printf "Safe: %d\n" (count_safe (make_reports testStr));;
 
+
+let read_file_to_strings filename =
+        let file = In_channel.open_text filename in
+        let strings = In_channel.input_lines file in
+        In_channel.close file; (List.fold_left (fun init x -> init ^ "\n" ^ x) (List.hd strings) (List.tl strings));;
+
+let () = printf "Safe: %d\n" (count_safe (make_reports (read_file_to_strings "data-p1.txt")));;
+
+let remove_1 l ind =
+        List.filteri (fun i _ -> ind <> i) l;;
+
+let report_is_safe_one_fault report : bool =
+        let res = report_is_safe report in
+        match res with
+        | true -> true
+        | false -> not (List.is_empty (List.filteri (fun i _ -> (report_is_safe (remove_1 report i))) report))
+
+
+let count_safe_one_fault reports = 
+        List.length (List.filter (fun r -> report_is_safe_one_fault r) reports);;
+
+let () = printf "Safe test Fault tolerant: %d\n" (count_safe_one_fault (make_reports testStr));;
+let () = printf "Safe line test Fault tolerant: %d\n" (count_safe_one_fault (make_reports "1 2 7 8 9"));;
+let () = printf "Safe test Fault tolerant: %d\n" (count_safe_one_fault (make_reports testStr));;
+let () = printf "Safe Fault tolerant: %d\n" (count_safe_one_fault (make_reports (read_file_to_strings "data-p1.txt")));;
