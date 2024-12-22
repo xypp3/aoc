e609013b47f6deb0c2c9da7b2f38a9759a875c28
xypp3 <petrkmilev@gmail.com>
2024-12-22 01:23:24 +0100

    day6 part 2

--- 2024/day_06/bin/main.ml
+++ 2024/day_06/bin/main.ml
@@ -44,8 +44,6 @@ let walk_to_end grid =
         let dir = (-1,0) in
         let max_x = max_x_grid grid in
         let max_y = max_y_grid grid in
-        printf "maxy:%d;maxx:%d;\n" max_y max_x;
-
         let next_dir dir = match dir with
                 | (-1,0) -> (0,1)
                 | (0,1) -> (1,0)
@@ -57,7 +55,6 @@ let walk_to_end grid =
                 let sy, sx = start in
                 let dy, dx = dir in
                 let y, x = (sy+dy),(sx+dx) in
-
                 if 0 <= y && y < (max_y) && 0 <= x && x < max_x then
                         try let _ = grid.(y).(x) in Some (y,x) with
                         | Invalid_argument _ -> printf "invalid pos"; None
@@ -71,26 +68,93 @@ let walk_to_end grid =
                 | Some _ -> printf "error when walking in line"; Some (420,420)
                 | None -> None
         in
-
         let rec walk start dir = 
                 match walk_in start dir with
                 | Some pos -> walk pos (next_dir dir)
                 | None -> ()
         in
         walk start dir;;
+let () =
+        (* let grid = make_grid txt_small_1 in *)
+        (* Array.iteri (fun i l -> printf "-"; Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid; *)
+        (* printf "\n\n"; *)
+        (* walk_to_end grid; *)
+        (* Array.iteri (fun i l -> printf "-"; Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid; *)
+        (* printf "Ans-small:%d\n" (List.length (find_char grid 'X')); *)
+        printf "we starting\n";
+        let grid = make_grid txt_big in
+        (* Array.iteri (fun i l -> printf "-"; Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid; *)
+        walk_to_end grid;
+        (* Array.iteri (fun i l -> Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid; *)
+        (* List.iter (fun (y,x) -> printf "%d,%d\n" y x) (find_char grid 'X'); *)
+        printf "Ans-big:%d\n" (List.length (find_char grid 'X'));;
 
+let detect_loop grid =
+        let start = List.nth (find_char grid '^') 0 in
+        let tmpy, tmpx = start in
+        grid.(tmpy).(tmpx) <- 'X';
+        let dir = (-1,0) in
+        let max_x = max_x_grid grid in
+        let max_y = max_y_grid grid in
+
+        let next_dir dir = match dir with
+                | (-1,0) -> (0,1)
+                | (0,1) -> (1,0)
+                | (1,0) -> (0,-1)
+                | (0,-1) -> (-1,0)
+                | _ -> printf "direction doesn't exist"; (0,0)
+        in
+        let get_next_pos start dir =
+                let sy, sx = start in
+                let dy, dx = dir in
+                let y, x = (sy+dy),(sx+dx) in
+
+                if 0 <= y && y < (max_y) && 0 <= x && x < max_x then
+                        try let _ = grid.(y).(x) in Some (y,x) with
+                        | Invalid_argument _ -> None
+                else
+                        None
+        in
+
+        let prev_pos_dir_tb = (Hashtbl.create 1000) in
+        let rec walk_in start dir =
+                let sy, sx = start in
+
+                match get_next_pos start dir with
+                | Some (y,x) when grid.(y).(x) = '.' -> grid.(y).(x) <- 'X'; walk_in (y,x) dir
+                | Some (y,x) when grid.(y).(x) = '#' || grid.(y).(x) = 'O' -> Some start
+                | Some (y,x) when grid.(y).(x) = 'X' ->
+
+                        (* WARN: use curr pos to check if visited not next pos*)
+                        (match Hashtbl.find_opt prev_pos_dir_tb ((sy,sx),dir) with
+                        | Some _ -> Some (-888,-888)
+                        | None -> walk_in (y,x) dir)
+                | Some _ -> printf "error when walking in line"; Some (420,420)
+                | None -> None
+        in
+
+        let rec walk start dir = 
+                match walk_in start dir with
+                | Some (-888,-888) -> 1
+                | Some pos -> (Hashtbl.add prev_pos_dir_tb (start,dir) true); walk pos (next_dir dir)
+                | None -> 0
+        in
+        walk start dir 
 
 let () =
-        let grid = make_grid txt_small_1 in
-        Array.iteri (fun i l -> printf "-"; Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid;
-        printf "\n\n";
-        walk_to_end grid;
-        Array.iteri (fun i l -> printf "-"; Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid;
-        printf "Ans-small:%d\n" (List.length (find_char grid 'X'));
+        printf "starting readin\n";
         let grid = make_grid txt_big in
-        Array.iteri (fun i l -> printf "-"; Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid;
-        walk_to_end grid;
-        Array.iteri (fun i l -> Array.iteri (fun j _ -> printf "%c" grid.(i).(j)) l; printf "\n") grid;
-        List.iter (fun (y,x) -> printf "%d,%d\n" y x) (find_char grid 'X');
-        printf "Ans-big:%d\n" (List.length (find_char grid 'X'));
-
+        let dup g = Array.map (fun l -> Array.copy l) g in
+        let count = ref 0 in
+        printf "starting looping\n";
+        Array.iteri (fun i l ->
+                l
+                |> Array.iteri (fun j c ->
+                        if c = '.' then
+                                let tmp = dup grid in
+                                tmp.(i).(j) <- 'O';
+                                count := !count + (detect_loop tmp);
+                                (* printf "%d:%d\n" i j; *)
+                );
+        ) grid;
+        printf "loops found:%d\n" !count;;
